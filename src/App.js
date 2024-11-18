94f147972c44a6464c9ebefa6fed591bca48d8a8
Vika <bazavova2001@mail.ru>
2024-11-18 14:43:08 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Базавова В.В";
+  const greeting = "Это обновленый текст для проверки диплоя";
 
   return (
     <div className="App">
